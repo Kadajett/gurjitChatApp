5d106b014d23fb86331d5a868fb0800b26772c25
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2015-02-27 00:49:47 -0800

    adds time since function

--- app/scripts/directives/time.js
+++ app/scripts/directives/time.js
@@ -14,6 +14,34 @@ angular.module('gurjitChatAppApp')
       link: function postLink(scope, element, attrs) {
         scope.timeText = attrs.messageTime;
         element.text(scope.timeText.toString());
+
+        function timeSince(date) {
+
+        var seconds = Math.floor((new Date() - date) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
       }
     };
   });
