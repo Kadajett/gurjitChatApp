54a9eb0656323493c4754c300b5992aa9f397e49
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2015-02-26 22:57:15 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gurjitChatAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gurjitChatAppApp
+ */
+angular.module('gurjitChatAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
