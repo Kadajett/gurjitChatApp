e5998a78ea55c4ea300c993e1f94a69914676a05
Jeet Dhillon <jitdhillon87@gurjit-singhs-MacBook-Pro.local>
2015-02-27 20:51:32 -0800

    added kdd enter keypress function in message and added custom image url also changed clear message functionality and scroll bottom on new message

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'firebase'
+    'firebase',
+    'ui.utils'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,14 +11,38 @@ angular.module('gurjitChatAppApp')
   .controller('MainCtrl', function ($scope, chatserver) {
     $scope.data = chatserver.data;
 
-    
+    $scope.init = function init(){
+        $scope.scrollBottom();
+        $scope.clearMessage();
+        $scope.data.$watch(function(){
+            $scope.scrollBottom()
+        });
+    }
 
     $scope.clearMessage = function(){
-    	$scope.messageObj = {
-	    	name: 'jeremy',
-	    	message: '',
-            time: null
-	}
+        if(!$scope.messageObj || $scope.messageObj.name == 'jeremy'){
+            $scope.messageObj = {
+            name: 'jeremy',
+            message: '',
+            time: null,
+            image: 'http://placehold.it/50/55C1E7/fff&text=U'
+    }
+
+
+        }else
+        {
+            $scope.messageObj.message ='';
+            $scope.messageObj.time= null;
+
+    
+
+        }
+    	
+    }
+    $scope.scrollBottom = function scrollBottom(){
+        var objDiv = document.getElementById("chatBox");
+           objDiv.scrollTop = objDiv.scrollHeight;
+
     }
 
     $scope.sendMessage = function(){
@@ -29,5 +53,7 @@ angular.module('gurjitChatAppApp')
     	 $scope.clearMessage();
 
     }
-    $scope.clearMessage();
+    
+    
+    $scope.init()
   });
